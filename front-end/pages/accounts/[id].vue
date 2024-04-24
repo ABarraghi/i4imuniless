5f12c4d0c5446ce29b3af8ba556cdf0f485fe927
abarraghi <abarraghi@gmail.com>
2024-04-24 14:07:32 -0400

    Update account implemented

--- front-end/pages/accounts/[id].vue
+++ front-end/pages/accounts/[id].vue
@@ -15,8 +15,13 @@
                 <div v-else>
                     <form @submit.prevent="updateStudent">
                         <div class="mb-3">
-                            <label>Account Number</label>
-                            <input type="text" v-model="this.serviceaddress_id" class="form-control"/>
+                            <label>Account Service Address ID</label>
+                            <input type="text" v-model="student.serviceaddress_id" class="form-control"/>
+                            <span class="text-danger" v-if="this.errorList.name">{{ this.errorList.name[0] }}</span>
+                        </div>
+                        <div class="mb-3">
+                            <label>Account Suffix</label>
+                            <input type="text" v-model="student.suffix" class="form-control"/>
                             <span class="text-danger" v-if="this.errorList.name">{{ this.errorList.name[0] }}</span>
                         </div>
                         <div class="mb-3">
@@ -25,14 +30,24 @@
                             <span class="text-danger" v-if="this.errorList.course">{{ this.errorList.course[0] }}</span>
                         </div>
                         <div class="mb-3">
-                            <label>Address</label>
-                            <input type="text" v-model="student.service_add" class="form-control"/>
+                            <label>Street</label>
+                            <input type="text" v-model="service_add.streetname" class="form-control"/>
                             <span class="text-danger" v-if="this.errorList.email">{{ this.errorList.email[0] }}</span>
                         </div>
                         <div class="mb-3">
-                            <label>Phone</label>
-                            <input type="text" v-model="student.phone" class="form-control"/>
-                            <span class="text-danger" v-if="this.errorList.phone">{{ this.errorList.phone[0] }}</span>
+                            <label>City</label>
+                            <input type="text" v-model="service_add.city" class="form-control"/>
+                            <span class="text-danger" v-if="this.errorList.email">{{ this.errorList.email[0] }}</span>
+                        </div>
+                        <div class="mb-3">
+                            <label>State</label>
+                            <input type="text" v-model="service_add.state" class="form-control"/>
+                            <span class="text-danger" v-if="this.errorList.email">{{ this.errorList.email[0] }}</span>
+                        </div>
+                        <div class="mb-3">
+                            <label>Zip</label>
+                            <input type="text" v-model="service_add.zip" class="form-control"/>
+                            <span class="text-danger" v-if="this.errorList.email">{{ this.errorList.email[0] }}</span>
                         </div>
                         <div class="mb-3">
                             <button type="submit" class="btn btn-primary">Update</button>
@@ -53,6 +68,7 @@ export default {
         return {
             studentId: '',
             student: {},
+            service_add: {},
             isLoading: false,
             isLoadingTitle: "Loading...",
             errorList: {}
@@ -60,7 +76,6 @@ export default {
     },
     mounted() {
         this.studentId = this.$route.params.id;
-        // alert(this.studentId);
 
         this.getStudent(this.studentId);
     },
@@ -71,11 +86,11 @@ export default {
             this.isLoading = true;
 
             axios.get(`https://jsjdf7f5di.execute-api.us-east-1.amazonaws.com/todos/acc_account/${studentId}`).then(res => {
-                console.log(res);
+                console.log(res.data[0]);
                 this.student = res.data[0];
                 axios.get(`https://jsjdf7f5di.execute-api.us-east-1.amazonaws.com/todos/acc_serviceaddress/${this.student.serviceaddress_id}`).then(res => {
-                                console.log(res.data[0]);
-                                this.student.service_add = `${res.data[0].streetname}, ${res.data[0].city} ${res.data[0].state} ${res.data[0].zip}`;
+                                this.service_add = res.data[0];
+                                
                             });
                 this.isLoading = false;
 
@@ -83,24 +98,49 @@ export default {
 
         },
 
+        
+
         updateStudent() {
             this.isLoading = true;
             this.isLoadingTitle = "Updating";
             // alert('saving student...')
 
-            var myThis = this;
-            axios.put(`https://jsjdf7f5di.execute-api.us-east-1.amazonaws.com/todos/acc_account/${studentId}`,this.student).then(res => {
+            console.log(this.student);
+            console.log(this.service_add);
 
-                console.log(res, 'res');
-                alert(res.data.message);
+            var myThis = this;
+
+            let acc_dict = {
+                "serviceaddress_id": `'${this.student.serviceaddress_id}'`,
+                "suffix": `'${this.student.suffix}'`,
+                "name": `'${this.student.name}'`
+            };
+
+            let sid_dict = {
+                "streetname": `'${this.service_add.streetname}'`,
+                "city": `'${this.service_add.city}'`,
+                "state": `'${this.service_add.state}'`,
+                "zip": `'${this.service_add.zip}'`
+            };
+
+            for(var key in acc_dict) {
+                this.student.updateColumn = key;
+                this.student.updateValue = acc_dict[key];
+
+                axios.put(`https://jsjdf7f5di.execute-api.us-east-1.amazonaws.com/todos/acc_account/${this.studentId}`, this.student).then(res => {
+
+                console.log(res);
+
+                // console.log(res, 'res');
+                // alert(res.data.message);
 
                 this.isLoading = false;
                 this.isLoadingTitle = "Loading";
 
                 this.errorList = {};
-                
-            })
-            .catch(function (error) {
+
+                })
+                .catch(function (error) {
                 console.log(error, 'errors');
 
                 if(error.response){
@@ -110,7 +150,39 @@ export default {
                 }
 
                 myThis.isLoading = false;
-            });
+                });
+            }
+
+            for(var key in sid_dict) {
+                this.service_add.updateColumn = key;
+                this.service_add.updateValue = sid_dict[key];
+
+                axios.put(`https://jsjdf7f5di.execute-api.us-east-1.amazonaws.com/todos/acc_serviceaddress/${this.student.serviceaddress_id}`, this.service_add).then(res => {
+
+                console.log(res);
+
+                // console.log(res, 'res');
+                // alert(res.data.message);
+
+                this.isLoading = false;
+                this.isLoadingTitle = "Loading";
+
+                this.errorList = {};
+
+                })
+                .catch(function (error) {
+                console.log(error, 'errors');
+
+                if(error.response){
+                    if(error.response.status == 422){
+                        myThis.errorList = error.response.data.errors;
+                    }
+                }
+
+                myThis.isLoading = false;
+                });
+            }
+
         }
     }
 }
